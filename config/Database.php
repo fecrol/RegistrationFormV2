de0faa87a5bd73ac34a8a30a1c8b5f0b9ce4ee32
Maciej Fec <maciejfec1996@gmail.com>
2022-05-16 21:39:28 +0100

    created a database class and added a method to connect to the database

--- config/Database.php
+++ config/Database.php
@@ -1,3 +1,37 @@
 <?php
 
+class Database {
+
+    private $host;
+    private $dbName;
+    private $username;
+    private $password;
+    private $conn;
+
+    public function __construct($dbCredentials) {
+
+        $this->host = $dbCredentials["host"];
+        $this->dbName = $dbCredentials["db_name"];
+        $this->username = $dbCredentials["username"];
+        $this->password = $dbCredentials["password"];
+    }
+
+    public function connect() {
+        /*
+        Establishes a connection with the database for queries.
+        */
+
+        $this->conn = null;
+
+        try {
+            $this->conn = mysqli_connect($this->host, $this->username, $this->password, $this->dbName);
+        }
+        catch(Exception $e) {
+            die("Could not connect to databse: " . mysqli_error());
+        }
+
+        return $this->conn;
+    }
+}
+
 ?>
