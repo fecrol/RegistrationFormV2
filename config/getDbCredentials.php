62f0874fb72cd441b99bb5192a36d441793b61ae
Maciej Fec <maciejfec1996@gmail.com>
2022-05-16 21:41:19 +0100

    script which returns the database credentials as json object.

--- config/getDbCredentials.php
+++ config/getDbCredentials.php
@@ -1,3 +1,12 @@
 <?php
 
+require_once "./DbCredentialsReader.php";
+
+$filePath = "./db.json";
+
+$dbCredentialsReader = new DbCredentialsReader();
+$dbCredentials = $dbCredentialsReader->getDbCredentials($filePath);
+
+echo json_encode($dbCredentials);
+
 ?>
