807cba52deb2856db5985163d01475e0a522f3b3
Maciej Fec <maciejfec1996@gmail.com>
2022-05-16 21:40:26 +0100

    created a class with a method to read the database credentials from db.json file

--- config/DbCredentialsReader.php
+++ config/DbCredentialsReader.php
@@ -1,3 +1,29 @@
 <?php
 
+class DbCredentialsReader {
+
+    private $dbCredentails;
+
+    public function __construct() {
+
+        $this->dbCredentials = null;
+    }
+
+    public function getDbCredentials($filePath) {
+        /*
+        Reads in database credentials from db.json to use for database connection.
+        */
+
+        if(file_exists($filePath)) {
+
+            $file = file_get_contents($filePath);
+            $this->dbCredentials = json_decode($file, true);
+
+            return $this->dbCredentials;
+        }
+
+        return $this->dbCredentials;
+    }
+}
+
 ?>
