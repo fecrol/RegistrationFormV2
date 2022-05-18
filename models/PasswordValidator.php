86477fef9f315c4c97ffb7041a2c4dd5eea7d491
Maciej Fec <maciejfec1996@gmail.com>
2022-05-18 18:34:13 +0100

    interface for password validation

--- models/PasswordValidator.php
+++ models/PasswordValidator.php
@@ -1,3 +1,18 @@
 <?php
 
+interface PasswordValidator {
+
+    public function validatePassword($password);
+    
+    public function validatePasswordLength($password);
+    
+    public function validatePasswordHasUpper($password);
+    
+    public function validatePasswordHasNum($password);
+    
+    public function validatePasswordHasSpecial($password);
+
+    public function validatePasswordsMatch($password, $confirmPassword);
+}
+
 ?>
