3a10ef413e7fbe2fb740f76fe7a524b5187ac778
Maciej Fec <maciejfec1996@gmail.com>
2022-05-18 16:41:07 +0100

    started work on a create request. connected to database and created a user object.

--- user/create.php
+++ user/create.php
@@ -1,3 +1,24 @@
 <?php
 
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    include_once "../config/DbCredentialsReader.php";
+    include_once "../config/Database.php";
+    include_once "../models/User.php";
+
+    $dbCredentialsFile = "../config/db.json";
+    $dbCredentialsReader = new DbCredentialsReader();
+    $dbCredentials = $dbCredentialsReader->getDbCredentials($dbCredentialsFile);
+
+    $database = new Database($dbCredentials);
+    $dbConn = $database->connect();
+
+    $forename = $_POST["forename"];
+    $surname = $_POST["surname"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    $user = new User($forename, $surname, $email, $password);
+}
+
 ?>
