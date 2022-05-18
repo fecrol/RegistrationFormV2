59bc469520a8189ed7df5d250d9a1939f3ce2a9f
Maciej Fec <maciejfec1996@gmail.com>
2022-05-18 18:44:47 +0100

    started work on regex validators for email, password and text

--- models/SignupFormValidator.php
+++ models/SignupFormValidator.php
@@ -1,3 +1,47 @@
 <?php
 
+require("StringValidator.php");
+require("EmailValidator.php");
+require("PasswordValidator.php");
+
+class SignupFormValidator implements StringValidator, EmailValidator, PasswordValidator {
+
+    public function validateString($string) {
+
+        $pattern = "/[^a-z]/i";
+        return preg_match($pattern, $string);
+    }
+    
+    public function validateEmail($email) {
+
+    }
+
+    public function validatePassword($password) {
+
+    }
+
+    public function validatePasswordLength($password) {
+
+    }
+
+    public function validatePasswordHasUpper($password) {
+
+    }
+
+    public function validatePasswordHasNum($password) {
+
+    }
+    
+    public function validatePasswordHasSpecial($password) {
+
+    }
+
+    public function validatePasswordsMatch($password, $confirmPassword) {
+
+    }
+}
+
+$s = new SignupFormValidator();
+$r = $s->validateString("Maciej");
+
 ?>
