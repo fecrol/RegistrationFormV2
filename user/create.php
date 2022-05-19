013d0c20364a5509acbda0aa1ea18276b269a1e2
Maciej Fec <maciejfec1996@gmail.com>
2022-05-19 16:17:02 +0100

    finished code for POST request to register a user

--- user/create.php
+++ user/create.php
@@ -1,24 +1,45 @@
 <?php
 
+include_once "../config/DbCredentialsReader.php";
+include_once "../config/Database.php";
+include_once "../models/User.php";
+include_once "../models/SignupFormValidator.php";
+
 if($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    include_once "../config/DbCredentialsReader.php";
-    include_once "../config/Database.php";
-    include_once "../models/User.php";
+    if(!empty($_POST["forename"]) && !empty($_POST["surname"]) && !empty($_POST["email"]) && !empty($_POST["password"]) && !empty($_POST["confirm-pass"])) {
 
-    $dbCredentialsFile = "../config/db.json";
-    $dbCredentialsReader = new DbCredentialsReader();
-    $dbCredentials = $dbCredentialsReader->getDbCredentials($dbCredentialsFile);
+        $sfv = new SignupFormValidator();
 
-    $database = new Database($dbCredentials);
-    $dbConn = $database->connect();
+        $forename = $_POST["forename"];
+        $surname = $_POST["surname"];
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+        $confirmPassword = $_POST["confirm-pass"];
 
-    $forename = $_POST["forename"];
-    $surname = $_POST["surname"];
-    $email = $_POST["email"];
-    $password = $_POST["password"];
+        $formIsValid = $sfv->validateForm($forename, $surname, $email, $password, $confirmPassword);
 
-    $user = new User($forename, $surname, $email, $password);
+        if($formIsValid) {
+
+            $dbCredentialsFile = "../config/db.json";
+            $dbCredentialsReader = new DbCredentialsReader();
+            $dbCredentials = $dbCredentialsReader->getDbCredentials($dbCredentialsFile);
+
+            $database = new Database($dbCredentials);
+            $dbConn = $database->connect();
+
+            $user = new User($forename, $surname, $email, $password);
+            $user->create($dbConn);
+
+            echo json_encode(["msg" => "success"]);
+        }
+        else {
+            echo json_encode(["msg" => "form contains invalid data!"]);
+        }
+    }
+    else {
+        echo json_encode(["msg" => "form contains empty field(s)!"]);
+    }
 }
 
 ?>
