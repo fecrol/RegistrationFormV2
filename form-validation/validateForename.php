75a6443dca7cb503d679677882d31f6fddcd697c
Maciej Fec <maciejfec1996@gmail.com>
2022-05-19 21:49:42 +0100

    processe POST request to validate forename and return a valid response.

--- form-validation/validateForename.php
+++ form-validation/validateForename.php
@@ -4,6 +4,26 @@ include_once "../models/SignupFormValidator.php";
 
 $sfv = new SignupFormValidator();
 
-$forename = $_POST["forename"];
+$msg;
+$data = json_decode(file_get_contents("php://input"));
+
+if(isset($data->forename)) {
+
+    $forename = $data->forename;
+
+    $forenameIsValid = $sfv->validateString($forename);
+
+    if($forenameIsValid && strlen($forename) > 0) {
+        $msg = true;
+    }
+    else {
+        $msg = false;
+    }
+}
+else {
+    $msg = "no data to be processed.";
+}
+
+echo json_encode(["msg" => $msg]);
 
 ?>
