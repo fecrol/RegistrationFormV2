fd5f7eef3e7d3fc99f33c2cff0f3610c08794780
Maciej Fec <maciejfec1996@gmail.com>
2022-05-20 21:42:56 +0100

    added code to handle POST request to validate email address.

--- form-validation/validateEmail.php
+++ form-validation/validateEmail.php
@@ -1,3 +1,29 @@
 <?php
 
+include_once "../models/SignupFormValidator.php";
+
+$sfv = new SignupFormValidator();
+
+$msg;
+$data = json_decode(file_get_contents("php://input"));
+
+if(isset($data->email)) {
+
+    $email = $data->email;
+
+    $emailIsValid = $sfv->validateEmail($email);
+
+    if($emailIsValid && strlen($email) > 0) {
+        $msg = true;
+    }
+    else {
+        $msg = false;
+    }
+}
+else {
+    $msg = "no data to be processed.";
+}
+
+echo json_encode(["msg" => $msg]);
+
 ?>
