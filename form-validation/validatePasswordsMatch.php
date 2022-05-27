53f475c0aa4c12b1060a382c7bd14a7529b3affd
Maciej Fec <maciejfec1996@gmail.com>
2022-05-27 11:24:28 +0100

    added code to handle POST request to validate if passwords match

--- form-validation/validatePasswordsMatch.php
+++ form-validation/validatePasswordsMatch.php
@@ -1,3 +1,30 @@
 <?php
 
+include_once "../models/SignupFormValidator.php";
+
+$sfv = new SignupFormValidator();
+
+$msg;
+$data = json_decode(file_get_contents("php://input"));
+
+if(isset($data->pass) && isset($data->confirmPass)) {
+
+    $pass = $data->pass;
+    $confirmPass = $data->confirmPass;
+
+    $passwordsMatch = $sfv->passwordsMatch($pass, $confirmPass);
+
+    if($passwordsMatch) {
+        $msg = true;
+    }
+    else {
+        $msg = false;
+    }
+}
+else {
+    $msg = "no data to be processed";
+}
+
+echo json_encode(["msg" => $msg]);
+
 ?>
