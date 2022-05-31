32f528f1f5a55e09e0ebcbdd22fc79f0dd442885
Maciej Fec <maciejfec1996@gmail.com>
2022-05-31 18:00:00 +0100

    added code to handle a post request to validate password

--- form-validation/validatePassword.php
+++ form-validation/validatePassword.php
@@ -1,3 +1,28 @@
 <?php
 
+include_once "../models/SignupFormValidator.php";
+
+$sfv = new SignupFormValidator();
+
+$msg;
+$data = json_decode(file_get_contents("php://input"));
+
+if(isset($data->password)) {
+
+    $password = $data->password;
+
+    $validPassLength = $sfv->validatePasswordLength($password);
+    $passHasUpper = $sfv->passwordHasUpper($password);
+    $passHasNum = $sfv->passwordHasNum($password);
+    $passHasApprovedSpecial = $sfv->passwordHasApprovedSpecial($password);
+    $passHasIllegalSpecial = $sfv->passwordHasIllegalSpecial($password);
+
+    $msg = ["validLength" => $validPassLength, "hasUpper" => $passHasUpper, "hasNum" => $passHasNum, "hasApprovedSpecial" => $passHasApprovedSpecial, "hasIllegalSpecial" => $passHasIllegalSpecial];
+}
+else {
+    $msg = "no data to be processed";
+}
+
+echo json_encode(["msg" => $msg]);
+
 ?>
