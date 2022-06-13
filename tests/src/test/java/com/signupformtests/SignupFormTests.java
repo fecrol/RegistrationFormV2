2165cedfe6c64d4a4c483109a00f96b11a2238e7
Maciej Fec <maciejfec1996@gmail.com>
2022-06-13 22:23:59 +0100

    added beasic set up and tear down methods

--- tests/src/test/java/com/signupformtests/SignupFormTests.java
+++ tests/src/test/java/com/signupformtests/SignupFormTests.java
@@ -1,6 +1,35 @@
 package com.signupformtests;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
 public class SignupFormTests {
 
+    public WebDriver driver;
+    public String baseUrl;
 
+    @BeforeSuite
+    public void setUp() {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\macie\\Desktop\\Programming\\WebDrivers\\chromedriver_win32\\chromedriver.exe");
+        driver = new ChromeDriver();
+        baseUrl = "http://localhost/_registrationForm/register.html";
+
+        driver.manage().window().maximize();
+        driver.get(baseUrl);
+    }
+
+    @Test
+    public void testEmptyForm() {
+
+    }
+
+    @AfterSuite
+    public void tearDown() {
+
+        driver.quit();
+    }
 }
