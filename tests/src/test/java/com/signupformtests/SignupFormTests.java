502ee0c78404dca33ac8153472e4ea3b009795ba
Maciej Fec <maciejfec1996@gmail.com>
2022-06-13 22:36:52 +0100

    added dummy test data

--- tests/src/test/java/com/signupformtests/SignupFormTests.java
+++ tests/src/test/java/com/signupformtests/SignupFormTests.java
@@ -10,9 +10,35 @@ public class SignupFormTests {
 
     public WebDriver driver;
     public String baseUrl;
+    
+    public String validForename;
+    public String validSurname;
+    public String validEmail;
+    public String validPassword;
+    public String validConfirmPassword;
+
+    public String invalidForename;
+    public String invalidSurname;
+    public String invalidEmail;
+    public String invalidPassword;
+    public String invalidConfirmPassword;
 
     @BeforeSuite
     public void setUp() {
+        
+        // Valid details
+        validForename = "Joe";
+        validSurname = "Blogs";
+        validEmail = "joe.blogs@email.com";
+        validPassword = "P@5sword";
+        validConfirmPassword = validPassword;
+        
+        // Invalid details
+        invalidForename = "J0e;";
+        invalidSurname = "3log5";
+        invalidEmail = "joe.blogs@email";
+        invalidPassword = "pass/";
+        invalidConfirmPassword = "/ssap";
 
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\macie\\Desktop\\Programming\\WebDrivers\\chromedriver_win32\\chromedriver.exe");
         driver = new ChromeDriver();
